a1c8206a2e11a78e82acc8b7138bdbe8d94be8e5
drschwenk <schwgit2@gmail.com>
2017-04-19 11:44:39 -0700

    replaced symlink test with file

--- test_eval.py
+++ test_eval.py
@@ -1,1 +1,68 @@
-../test_eval/test_eval.py
+import pytest
+import os.path
+from vuc_task_evaluation.score_submission import EvaluatorSelector
+
+
+tqa_base_path = './test/tqa'
+charades_base_path = './test/charades'
+thor_base_path = './test/thor'
+tqa_gt_path = os.path.join(tqa_base_path, 'val_ans.json')
+charades_gt_path = os.path.join(charades_base_path, 'Charades_v1_test.csv')
+
+
+
+@pytest.fixture
+def evaluator(submission, gt_path):
+    submission_evaluator = EvaluatorSelector.get_submission_specific_evaluator(submission, gt_path)
+    return submission_evaluator
+
+
+###
+#TQA tests here
+###
+@pytest.mark.parametrize("submission, gt_path, expected", [
+        (os.path.join(tqa_base_path, 'random.json'), tqa_gt_path, (0.23299050632911392, 0.24487594390507011)),
+        (os.path.join(tqa_base_path, 'ndq_only.json'), tqa_gt_path, (1.0, 0)),
+        (os.path.join(tqa_base_path, 'dq_only.json'), tqa_gt_path, (0.0, 1.0)),
+        (os.path.join(tqa_base_path, 'perfect.json'), tqa_gt_path, (1.0, 1.0))])
+def test_tqa_submissions(evaluator, submission, expected):
+    accuracies = evaluator.evaluate_submission()
+    print(list(accuracies))
+    assert accuracies[0][1] == expected[0]
+    assert accuracies[1][1] == expected[1]
+
+
+@pytest.mark.parametrize("submission, gt_path, expected", [
+    # (os.path.join(tqa_base_path, 'missing_vals.json'), tqa_gt_path, AssertionError), changed the task, not needed now
+    (os.path.join(tqa_base_path, 'extra_vals.json'), tqa_gt_path, AssertionError),
+    (os.path.join(tqa_base_path, 'malformed_bad_prefix.json'), tqa_gt_path, AssertionError),
+    (os.path.join(tqa_base_path, 'malformed_bad_id_number.json'), tqa_gt_path, AssertionError),
+    (os.path.join(tqa_base_path, 'malformed_bad_answer.json'), tqa_gt_path, AssertionError)])
+def test_tqa_structural(evaluator, submission, gt_path, expected):
+    with pytest.raises(expected):
+        evaluator.evaluate_submission()
+
+
+###
+# Charades tests here
+###
+@pytest.mark.parametrize("submission, gt_path, expected", [
+    (os.path.join(charades_base_path, 'charades_class_sample.txt'), charades_gt_path, 0.18614618839020669),
+    (os.path.join(charades_base_path, 'charades_loc_sample.txt'), charades_gt_path, 0.089389146288006677),
+    (os.path.join(charades_base_path, 'charades_loc_scrambled.txt'), charades_gt_path, 0.089389146288006677),
+    (os.path.join(charades_base_path, 'charades_class_scrambled.txt'), charades_gt_path, 0.18614618839020669),
+    ])
+def test_charades_submissions(evaluator, submission, gt_path, expected):
+    mean_ap = evaluator.evaluate_submission()
+    assert mean_ap[1] == expected
+
+
+@pytest.mark.parametrize("submission, gt_path, expected", [
+    (os.path.join(charades_base_path, 'charades_loc_missing.txt'), charades_gt_path, AssertionError),
+    (os.path.join(charades_base_path, 'charades_class_missing.txt'), charades_gt_path, AssertionError),
+    (os.path.join(charades_base_path, 'charades_class_obj_class_missing.txt'), charades_gt_path, AssertionError),
+    (os.path.join(charades_base_path, 'charades_loc_obj_class_missing.txt'), charades_gt_path, AssertionError),
+    ])
+def test_charades_structural(evaluator, submission, gt_path, expected):
+    with pytest.raises(expected):
+        evaluator.evaluate_submission()
